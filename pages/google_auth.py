90b5a00bd5dd176a5990ac998482e043d69766aa
n-howard <nhoward@oberlin.edu>
2025-07-02 10:55:30 -0500

    debugging

--- pages/google_auth.py
+++ pages/google_auth.py
@@ -34,7 +34,7 @@ def login():
         )
 
         st.session_state.oauth_state = state
-
+        st.html(f"[Click here to log in with Google]({auth_url})")
         
         st.html(f"""
         <style>
