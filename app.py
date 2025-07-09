c681c7dba64620bd9e5cb3dde59e9dad19b572b7
n-howard <nhoward@oberlin.edu>
2025-07-09 10:24:44 -0500

    login button

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="INSIGHT", layout="wide", page_icon="./oask_short_logo.png", initial_sidebar_state="collapsed")
 if not hasattr(st, "user") or not st.user.is_logged_in:
     st.session_state["__waiting_for_auth"] = True
-    st.login("auth0")
-    st.stop()
+    if st.button("Sign In"):
+        st.login("auth0")
+        st.stop()
 elif st.session_state.get("__waiting_for_auth"):
     # Prevent infinite loop during transition from login to page rerun
     del st.session_state["__waiting_for_auth"]
@@ -131,10 +132,10 @@ state = st.query_params.get("state") or st.session_state.get("oauth_state") or c
 #         login()
 #         st.stop()
 
-if not st.user.is_logged_in:
-    if st.button("Sign In"):
-        st.login("auth0")
-        st.stop()
+# if not st.user.is_logged_in:
+#     if st.button("Sign In"):
+#         st.login("auth0")
+#         st.stop()
 
 
 
